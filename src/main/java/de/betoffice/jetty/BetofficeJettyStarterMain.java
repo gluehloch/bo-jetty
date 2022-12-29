1ce7700f6441b74f50c4003f47677e1ac82913e1
Andre Winkler <gluehloch@googlemail.com>
2022-12-29 07:21:09 +0100

    Trying...

--- src/main/java/de/betoffice/jetty/BetofficeJettyStarterMain.java
+++ src/main/java/de/betoffice/jetty/BetofficeJettyStarterMain.java
@@ -2,11 +2,14 @@ package de.betoffice.jetty;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.NoJspServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -16,7 +19,7 @@ public class BetofficeJettyStarterMain {
 
     private static final int PORT = 9290;
 
-    private static final String CONTEXT_PATH = "/";
+    private static final String CONTEXT_PATH = "/bo";
     private static final String CONFIG_LOCATION_PACKAGE = "de.betoffice";
     private static final String MAPPING_URL = "/";
     private static final String WEBAPP_DIRECTORY = "WEB-INF"; // "webapp";
@@ -26,7 +29,7 @@ public class BetofficeJettyStarterMain {
     }
 
     private void startJetty(int port) throws Exception {
-        System.out.printf("Starting server at port {}", port);
+        System.out.printf("Starting server at port %s", port);
         Server server = new Server(port);
 
         server.setHandler(getServletContextHandler());
@@ -34,34 +37,40 @@ public class BetofficeJettyStarterMain {
         addRuntimeShutdownHook(server);
 
         server.start();
-        System.out.printf("Server started at port {}", port);
+        System.out.printf("Server started at port %s", port);
         server.join();
     }
 
     private static ServletContextHandler getServletContextHandler() throws IOException {
-        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS); 
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         contextHandler.setErrorHandler(null);
 
-        contextHandler.setResourceBase(new ClassPathResource(WEBAPP_DIRECTORY).getURI().toString());
-        contextHandler.setContextPath(CONTEXT_PATH);
+        contextHandler.setContextPath("/ping");
+        contextHandler.addServlet(new ServletHolder(new HelloServlet()), "/*");
+        //contextHandler.setResourceBase(new ClassPathResource(WEBAPP_DIRECTORY).getURI().toString());
+        //contextHandler.setContextPath(CONTEXT_PATH);
 
         // JSP
         // contextHandler.setClassLoader(Thread.currentThread().getContextClassLoader());
         // contextHandler.addServlet("jsp", J);
 
         // Spring
+        /*
         WebApplicationContext webAppContext = getWebApplicationContext();
         DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppContext);
-        ServletHolder springServletHolder = new ServletHolder("mvc-dispatcher", dispatcherServlet);
+        ServletHolder springServletHolder = new ServletHolder("bo", dispatcherServlet);
         contextHandler.addServlet(springServletHolder, MAPPING_URL);
+        contextHandler.getInitParams().put("contextConfigLocation", "classpath:/betoffice.xml");
         contextHandler.addEventListener(new ContextLoaderListener(webAppContext));
+        */
 
         return contextHandler;
     }
 
     private static WebApplicationContext getWebApplicationContext() {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.setConfigLocation(CONFIG_LOCATION_PACKAGE);
+        context.scan(CONFIG_LOCATION_PACKAGE);
+        context.setConfigLocation("classpath:/betoffice.xml");
         return context;
     }
 
@@ -80,6 +89,17 @@ public class BetofficeJettyStarterMain {
                 }
             }
         }));
+    }
+
+    public static class HelloServlet extends HttpServlet {
+        private static final long serialVersionUID = -6154475799000019575L;
+        private static final String greeting = "Hello World";
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(greeting);
+        }
     }
 
 }
